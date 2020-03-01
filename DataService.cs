92de5670274e16288e04abaeaed19f38486f6976
Sean Davis <sean.dev.davis@gmail.com>
2020-02-29 19:33:26 -0600

    added heros, villans, and matches

--- DataService.cs
+++ DataService.cs
@@ -5,24 +5,61 @@ using System.Threading.Tasks;
 
 namespace HeroProject.Web
 {
-    public class Hero
+  public class Hero
+  {
+    public string Name { get; set; }
+  }
+
+  public class Villan
+  {
+    public string Name { get; set; }
+  }
+
+  public class Match
+  {
+    public Hero Hero { get; set; }
+    public Villan Villan { get; set; }
+    public string Winner { get; set; }
+  }
+
+  public class DataService
+  {
+    List<Hero> _heros = new List<Hero>();
+    List<Villan> _villan = new List<Villan>();
+
+    List<Match> _match = new List<Match>();
+
+    public DataService()
     {
-        public string Name { get; set; }
+      _heros.Add(new Hero { Name = "Superman" });
+      _heros.Add(new Hero { Name = "Batman" });
+      _heros.Add(new Hero { Name = "Ironman" });
+      _heros.Add(new Hero { Name = "Black Panther" });
+      _heros.Add(new Hero { Name = "The Flash" });
+      _heros.Add(new Hero { Name = "Spiderman" });
+      _villan.Add(new Villan { Name =  "Lex Luthor" });
+      _villan.Add(new Villan { Name =  "The Penguin" });
+      _villan.Add(new Villan { Name =  "Joker" });
+      _villan.Add(new Villan { Name =  "Poison Ivy" });
+      _villan.Add(new Villan { Name =  "Zoom" });
+      _villan.Add(new Villan { Name =  "Green Goblin" });
+      _match.Add(new Match {Name = "Superman", Name = "Lex Luthor", Winner = "Superman"});
+      _match.Add(new Match {Name = "Spiderman", Name = "Green Goblin", Winner = "Spiderman"});
+      _match.Add(new Match {Name = "The Flash", Name = "Zoom", Winner = "Zoom"});
+      _match.Add(new Match {Name = "Black Panther", Name = "The Penguin", Winner = "Black Panther"});
+      _match.Add(new Match {Name = "Batman", Name = "Joker", Winner = "Joker"});
     }
 
-    public class DataService
+    public List<Hero> GetHeros()
     {
-        List<Hero> _heros = new List<Hero>();
-
-        public DataService()
-        {
-            _heros.Add(new Hero { Name = "Superman" });
-            _heros.Add(new Hero { Name = "Batman" });
-        }
-
-        public List<Hero> GetHeros()
-        {
-            return _heros;
-        }
+      return _heros;
+      return _villan;
     }
+
+    public boolean HaveTheyFaught(Hero hero, Villan villan)
+    {
+        return _match;
+    }
+  }
 }
+
