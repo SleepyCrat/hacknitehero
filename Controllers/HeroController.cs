1b5124fd431a80953df7a9cb59756ae6d66715bf
SleepyCrat <frankjones04@gmail.com>
2020-02-29 18:23:36 -0600

    inital

---
+++ Controllers/HeroController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeroProject.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroController : Controller
+    {
+        DataService _dataService;
+
+        public HeroController(DataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        [HttpGet("")]
+        
+        public List<Hero> Index()
+        {
+            return _dataService.GetHeros();
+        }
+    }
+}
---
+++ DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeroProject.Web
+{
+    public class Hero
+    {
+        public string Name { get; set; }
+    }
+
+    public class DataService
+    {
+        List<Hero> _heros = new List<Hero>();
+
+        public DataService()
+        {
+            _heros.Add(new Hero { Name = "Superman" });
+            _heros.Add(new Hero { Name = "Batman" });
+        }
+
+        public List<Hero> GetHeros()
+        {
+            return _heros;
+        }
+    }
+}
